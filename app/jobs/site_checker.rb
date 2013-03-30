d95949105a8528de53f342dc7740485f85c3ccfd
Francis Chong <francis@ignition.hk>
2013-03-30 11:42:37 +0800

    add docs

--- app/jobs/site_checker.rb
+++ app/jobs/site_checker.rb
@@ -2,18 +2,10 @@ require_relative "../models/site"
 require_relative "../models/site_watcher"
 
 class SiteChecker
-  # Check status of a site
-  # 
-  # url - url for the site to be fetched
-  # hydra - Typhoeus Hydra object, by default create a new hydra
-  # 
-  # Returns hydra request, caller should run the hydra to fire the check
-  def self.check_url(url, timeout=15, hydra=Typhoeus::Hydra.new)
-    site = Site.where(:url => url).first || Site.create(:url => url)
-    SiteWatcher.new(site, timeout, hydra).watch
-  end
-
   # Check all status
+  # urls - array of URL
+  # timeout - http timeout, in second, default 15
+  # concurrency - http concurrency, defautl 20
   def self.check_urls(urls, timeout=15, concurrency=20)
     hydra = Typhoeus::Hydra.new(:max_concurrency => concurrency)
     requests = Settings.sites.collect do |url|
@@ -23,5 +15,17 @@ class SiteChecker
     requests.collect do |r|
       r.response.handled_response
     end
+  end
+
+  private
+  # Check status of a site
+  # 
+  # url - url for the site to be fetched
+  # hydra - Typhoeus Hydra object, by default create a new hydra
+  # 
+  # Returns hydra request, caller should run the hydra to fire the check
+  def self.check_url(url, timeout=15, hydra=nil)
+    site = Site.where(:url => url).first || Site.create(:url => url)
+    request = SiteWatcher.new(site, timeout, hydra).watch
   end
 end
