f42b40481f6dd4e555f5292dc2a0dd834dda64c6
Francis Chong <francis@ignition.hk>
2013-03-30 11:45:31 +0800

    add email notification

--- app/models/site_watcher.rb
+++ app/models/site_watcher.rb
@@ -1,8 +1,12 @@
+require 'typhoeus'
+require_relative '../mailers/sites_mailer'
+
 class SiteWatcher
-  def initialize(site, timeout=15, hydra=Typhoeus::Hydra.hydra)
+  def initialize(site, timeout=15, hydra=Typhoeus::Hydra.hydra, mailer=SitesMailer)
     @hydra = hydra
     @site = site
     @http_timeout = timeout
+    @mailer = mailer
   end
 
   # use typhoeus to make a request and wait for response
@@ -12,6 +16,9 @@ class SiteWatcher
     # Create a request and on complete, update site status
     @request = Typhoeus::Request.new(@site.url, :method => :get, :timeout => @http_timeout * 1000, :followlocation => true)
     @request.on_complete  do |response|
+      last_failed = @site.failed?
+
+      # update status
       if response.success?
         @site.ok!
       elsif response.timed_out?
@@ -26,6 +33,14 @@ class SiteWatcher
       end
       @site.last_response_time = response.total_time
       @site.save!
+
+      # email notification
+      if last_failed && @site.ok?
+        mailer.notify_resolved(@site.id).deliver
+      elsif !last_failed && @site.failed?
+        mailer.notify_error(@site.id).deliver
+      end
+
       @site
     end
     @hydra.queue(@request)
